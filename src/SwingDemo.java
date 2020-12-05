391b54f4880c999794ae7baaed23a34134f805b1
lilac rain <2211261685@qq.com>
2020-12-05 16:14:51 +0800

    Initial commit

---
+++ src/SwingDemo.java
@@ -0,0 +1,39 @@
+package my;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SwingDemo
+{
+    private static void createGUI()
+    {
+        // JFrame指一个窗口，构造方法的参数为窗口标题
+        // 语法：因为MyFrame是JFrame的子类，所以可以这么写
+        JFrame frame = new MyFrame("Swing Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        // 设置窗口的其他参数，如窗口大小
+        frame.setSize(400, 300);
+
+        // 显示窗口
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        // 此段代码间接地调用了 createGUI()，具体原理在 Swing高级篇 里讲解
+        // 初学者先照抄此代码框架即可
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run()
+            {
+                createGUI();
+            }
+        });
+
+    }
+}
