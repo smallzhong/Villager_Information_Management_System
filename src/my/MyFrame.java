12dbd7b74c4504621d296cf3f408ace79862013d
lilac rain <2211261685@qq.com>
2020-12-12 20:59:13 +0800

    删除过时的sql连接测试代码

--- src/my/MyFrame.java
+++ src/my/MyFrame.java
@@ -30,54 +30,6 @@ public class MyFrame extends JFrame
     // 显示表格
     JTable table = new JTable(tableModel);
 
-    void testsql()
-    {
-        Connection conn = null;
-        Statement stmt = null;
-        try
-        {
-            // 注册 JDBC 驱动
-            Class.forName(JDBC_DRIVER);
-
-            // 打开链接
-            conn = DriverManager.getConnection(DB_URL, SQL_USER, SQL_PASS);
-
-            // 执行查询
-            stmt = conn.createStatement();
-            String sql;
-            sql = "delete from yc_villagers";
-            stmt.execute(sql);
-
-            // 完成后关闭
-            stmt.close();
-            conn.close();
-        } catch (SQLException se)
-        {
-            // 处理 JDBC 错误
-            se.printStackTrace();
-        } catch (Exception e)
-        {
-            // 处理 Class.forName 错误
-            e.printStackTrace();
-        } finally
-        {
-            // 关闭资源
-            try
-            {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2)
-            {
-            }// 什么都不做
-            try
-            {
-                if (conn != null) conn.close();
-            } catch (SQLException se)
-            {
-                se.printStackTrace();
-            }
-        }
-    }
-
     void init()
     {
 
